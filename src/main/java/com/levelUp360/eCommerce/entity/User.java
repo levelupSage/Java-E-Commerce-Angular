188530c63be5b5bdfff88e80f8ee11a58c8fbf68
Level_Up_ 360 <dronakapil@gmail.com>
2024-10-12 17:32:12 +0530

    updates

--- src/main/java/com/levelUp360/eCommerce/entity/User.java
+++ src/main/java/com/levelUp360/eCommerce/entity/User.java
@@ -4,12 +4,17 @@ import com.levelUp360.eCommerce.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Table(name="users", schema = "ecommerce")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +27,36 @@ public class User {
     private String password;
 
     private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 
 //    @Lob
 //    @Column(name = "img", columnDefinition = "bytea")
--- src/main/java/com/levelUp360/eCommerce/utils/JwtUtil.java
+++ src/main/java/com/levelUp360/eCommerce/utils/JwtUtil.java
@@ -16,71 +16,71 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-@Component
-public class JwtUtil {
+//@Component
+//public class JwtUtil {
+//
+//    private String secretKey = "cF781A";
+//
+//    public JwtUtil(){
+//        KeyGenerator keyGen = null;
+//        try {
+//            keyGen = KeyGenerator.getInstance("HmacSHA256");
+//            SecretKey sk = keyGen.generateKey();
+//            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+//        } catch (NoSuchAlgorithmException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public String generationToken(String userName){
+//        Map<String, Object> claims = new HashMap<>();
+//        return createToken(claims, userName);
+//    }
+//
+//    private String createToken(Map<String, Object> claims, String userName) {
+//        return Jwts.builder()
+//                .claims()
+//                .add(claims).subject(userName)
+//                .issuedAt(new Date(System.currentTimeMillis()))
+//                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+//                .and()
+//                .signWith(getSignKey())
+//                .compact();
+//    }
+//
+//    private SecretKey getSignKey() {
+//        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+//        return Keys.hmacShaKeyFor(keyBytes);
+//    }
+//
+//    public String extractUserName(String token){
+//        return extractClaims(token, Claims::getSubject);
+//    }
+//
+//    private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+//       final Claims claims = extractAllClaims(token);
+//       return claimsResolver.apply(claims);
+//    }
+//
+//    private Claims extractAllClaims(String token) {
+//        return Jwts.parser().verifyWith(getSignKey())
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload();
+//    }
+//
+//    private Boolean  isTokenExpired(String token){
+//        return extractExpiration(token).before(new Date());
+//    }
+//
+//    private Date extractExpiration(String token) {
+//        return extractClaims(token, Claims::getExpiration);
+//
+//    }
+//
+//    public Boolean validationToken(String token, UserDetails userDetails){
+//        final String userName = extractUserName(token);
+//        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+//    }
 
-    private String secretKey = "cF781A";
-
-    public JwtUtil(){
-        KeyGenerator keyGen = null;
-        try {
-            keyGen = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey sk = keyGen.generateKey();
-            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public String generationToken(String userName){
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userName);
-    }
-
-    private String createToken(Map<String, Object> claims, String userName) {
-        return Jwts.builder()
-                .claims()
-                .add(claims).subject(userName)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
-                .and()
-                .signWith(getSignKey())
-                .compact();
-    }
-
-    private SecretKey getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
-    public String extractUserName(String token){
-        return extractClaims(token, Claims::getSubject);
-    }
-
-    private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
-       final Claims claims = extractAllClaims(token);
-       return claimsResolver.apply(claims);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().verifyWith(getSignKey())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-    }
-
-    private Boolean  isTokenExpired(String token){
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaims(token, Claims::getExpiration);
-
-    }
-
-    public Boolean validationToken(String token, UserDetails userDetails){
-        final String userName = extractUserName(token);
-        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
-}
+//}
